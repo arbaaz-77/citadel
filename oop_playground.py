86afbe5d9033dd1204c70eb7d16ffdad8dbee32e
Arbaaz <arbaaz970@gmail.com>
2026-07-13 12:12:24 +0300

    feat:move archive behaviour into Archive class

--- oop_playground.py
+++ oop_playground.py
@@ -9,6 +9,7 @@ class Character:
     def introduce(self):
         print(f"I am {self.name}. \nI belong to House {self.house}. \nMy title is {self.title}.")
 
+
 class Archive:
     def __init__(self, characters=None):
         if characters is None:
@@ -16,12 +17,21 @@ class Archive:
 
         self.characters = characters
 
+    def view_characters(self):
+        for character in self.characters:
+            character.introduce()
+
+
+    def add_character(self, character):
+        self.characters.append(character)
+
+
 
 jon = Character("Jon Snow", "Stark", "King in the North")
 tyrion = Character("Tyrion Lannister", "Lannister", "Hand of the King")
 arya = Character("Arya Stark", "Stark", "No One")
+brienne = Character("Brienne of Tarth", "Tarth", "Lord Commander of the Kingsguard")
 
 archive = Archive([jon, tyrion, arya])
+archive.add_character(brienne)
 
-for character in archive.characters:
-    character.introduce()
